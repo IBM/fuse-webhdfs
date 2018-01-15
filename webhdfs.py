87bd9d0036c4e946cb3ecfa5d5f279393e888b47
Sasa Tomic <sat@zurich.ibm.com>
2018-01-15 15:16:35 +0100

    Better exception handling - catch the TypeError as well

--- webhdfs.py
+++ webhdfs.py
@@ -39,7 +39,7 @@ def get_auth():
     username = password = None
     try:
         username, account, password = netrc().authenticators(cfg['DEFAULT']['HDFS_HOST'])
-    except NetrcParseError:
+    except (NetrcParseError, TypeError):
         pass
     if not username:
         username = cfg['DEFAULT'].get('HDFS_USERNAME', "")
